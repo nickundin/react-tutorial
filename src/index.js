1c871f57ae4ecabaf30a2f19b9665518c897d157
Nicholas Kundin <nickundin@gmail.com>
2022-12-29 09:21:59 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import App from "./App";
+
+// the JS here is transformed into a functioning webpage
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
