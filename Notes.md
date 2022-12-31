c0f0431768dc07298089b14ae7b1a062b71a6713
Nicholas Kundin <nickundin@gmail.com>
2022-12-31 09:48:23 -0500

    Completed section 5

---
+++ Notes.md
@@ -0,0 +1,6 @@
+**Quiz 3**
+
+1. `someArray.map((element) => <p>{element}</p>)`
+   transforms an array (someArray) into a new array full of JSX elements (which can be output by React)
+2. You should the special "key" prop to list JSX elements because it's required for React to correctly identigy and update (if needed) the list elements.
+3. With regards to outputting conditional content in React components, you can work with regular ternary expressions or if checks to output or return different results in/from your component.
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,6 +2,9 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value); // creates an array of numbers
+  const totalMaximum = Math.max(...dataPointValues); // spreads the array into the argument values
+
   // the data points should be received as props
   return (
     <div className="chart">
@@ -9,7 +12,7 @@ const Chart = (props) => {
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.vale}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
